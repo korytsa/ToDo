759854844fa7f41563bef36b378ef2af73cac9e4
Анна Корытько <anni_korytko@MacBook-Air-Anna.local>
2022-07-12 17:44:10 +0300

    add buttons

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-class ToDOList{
-    constructor(){
+const addBtn = document.querySelector('#add');
+const removeBtn = document.querySelector('#remove');
+const sortBtn = document.querySelector('#sort');
+const clearBtn = document.querySelector('#clear');
+const list = document.querySelector('#list');
+
+class ToDOList {
+    constructor() {
         this.todo = []
     }
-    addToList(text){
-        const obj = {text: text, date: Date.now(), id: Math.floor(Math.random() * 100)};
-        this.todo.push(obj);
-    }
-    removeFromList(idArg){
-        let index = this.todo.findIndex((item) => {
-            return item.id === idArg.id
+    render() {
+        list.innerHTML = '';
+
+        this.todo.forEach((item) => {
+            let li = document.createElement('li');
+            li.innerText = `${item.text}  id: ${item.id}`;
+            list.appendChild(li);
         })
-        if (index !== -1){
+    }
+    addToList() {
+        let text = prompt('Enter your task');
+        const obj = {
+            text: text,
+            date: Date.now(),
+            id: Math.floor(Math.random() * 100)
+        };
+        this.todo.push(obj);
+        this.render();
+    }
+    removeFromList() {
+        let idArg = Number(prompt('Enter id for remove'));
+
+        let index = this.todo.findIndex((item) => {
+            return item.id === idArg
+        })
+
+        if (index !== -1) {
             this.todo.splice(index, 1)
         }
+        this.render();
     }
-    sort(date){
+    sort(date) {
         this.todo.sort((a, b) => {
-            return a.date - b.date
+            return b.date - a.date
         })
+        this.render();
     }
-    clear(){
+    clear() {
         this.todo.splice(0, this.todo.length);
+        this.render();
     }
 }
 
 const todo = new ToDOList();
-todo.addToList("Go to the Gym"); 
-todo.addToList("Call with Mascha"); 
-todo.removeFromList({id: 5}) 
-todo.sort(); 
-// todo.clear(); 
-console.log(todo)
+
+addBtn.addEventListener('click', () => {
+    todo.addToList();
+})
+removeBtn.addEventListener('click', () => {
+    todo.removeFromList();
+})
+sortBtn.addEventListener('click', () => {
+    todo.sort();
+})
+clearBtn.addEventListener('click', () => {
+    todo.clear();
+})
